925fc77665667ccc188a03199b5bb35546e3a37f
aadibansal <adit.nitd@gmail.com>
2016-08-31 22:54:54 +0530

    second

--- Assignment 1/todo.js
+++ Assignment 1/todo.js
@@ -4,7 +4,15 @@ angular.module('todoApp', [])
     todoList.todos = [];
  
     todoList.addTodo = function() {
+	  if(todoList.todoText.trim() == "")
+		  return;
       todoList.todos.push(todoList.todoText);
       todoList.todoText = '';
     };
+	
+	todoList.remove = function(index) {
+		console.log("jnkjn");
+		console.log(index);
+		todoList.todoText.splice(index,1);
+	}
  });
