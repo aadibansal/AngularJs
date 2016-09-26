14aa4a29b61b2088e0cf0e47b01a5aff0d0d892a
aadibansal <adit.nitd@gmail.com>
2016-09-26 21:06:57 +0530

    Project

--- Sale App/app/MyBootstrap/bootstrap.js
+++ Sale App/app/MyBootstrap/bootstrap.js
@@ -13,10 +13,37 @@ angular.module('myApp.bootstrap', ['ngRoute', 'paypalButtonApp', 'appConfig'])
             }
         }
     }
+  })
+  .when('/detail/:img/:name', {   
+    templateUrl: 'MyBootstrap/template_detail.html',
+    controller: 'detailCtrl',
+    resolve: {
+        check: function(checkLogIn, $location){
+            var data = checkLogIn.get();   
+            if(data!=true){
+               $location.path('/');
+            }
+        }
+    }
   });
 }])
 
+.controller('detailCtrl', ['$routeParams', '$scope', 'LogOut', '$location', '$rootScope', 'checkLogIn', '$anchorScroll', function($routeParams, $scope, LogOut, $location, $rootScope, checkLogIn, $anchorScroll){
+   $scope.objData = {};
+   console.log($routeParams.img);
+   $scope.objData.imgPath = $routeParams.img;
+    $scope.objData.name = $routeParams.name;
+      $location.hash("detailShow");
+    $anchorScroll();
+    $scope.logOut = function(){
+            LogOut.save().$promise.then(function(){
+                checkLogIn.set(false);
+                $location.url('/');
+            })
+       };
+}])
 .controller('bootCtrl', ['BootData','$scope', 'LogOut', '$location', '$rootScope', 'checkLogIn', '$anchorScroll', 'displayName', 'Users', function(BootData, $scope, LogOut, $location, $rootScope, checkLogIn, $anchorScroll, displayName, Users) {
+       
        $scope.bootData = BootData.query();
         $scope.dispName = "";
         var uid = displayName.get();   
@@ -34,5 +61,9 @@ angular.module('myApp.bootstrap', ['ngRoute', 'paypalButtonApp', 'appConfig'])
        $scope.top = function(){
             $location.hash("top");
             $anchorScroll();
-       }
+       };
+       $scope.detail = function(index){
+            console.log($scope.bootData[index]);
+            $location.url("/detail/"+$scope.bootData[index].imgPath+"/"+$scope.bootData[index].name);
+       };
 }]);
